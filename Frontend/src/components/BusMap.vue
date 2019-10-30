af92e90e5a31045b01a2465e0bdd3bef5062f420
adKoch <adam.kochanski.97@gmail.com>
2019-10-30 19:41:18 +0100

    Add custom MarkerCluster icons

--- Frontend/src/components/BusMap.vue
+++ Frontend/src/components/BusMap.vue
@@ -2,10 +2,13 @@
     <l-map :zoom="zoom" :center="center">
         <l-icon-default :image-path="'/statics/leafletImages/'"></l-icon-default>
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-        <l-marker-cluster :options="{animateAddingMarkers:false, animate:false, maxClusterRadius:60}" @spiderfied="1">
+        <l-marker-cluster :options="{animateAddingMarkers:false, animate:false, maxClusterRadius:60, iconCreateFunction:vehicleMarkerClusterIcon}" :icon="busIcon">
             <template v-for="vehicle in vehicles">
-                <l-marker v-if="vehicle.type===1" v-bind:key="vehicle.id" :lat-lng="getLatLngFromVehicleInfo(vehicle)"
-                          v-bind:icon="busIcon">
+                <l-marker v-if="vehicle.type===1"
+                          :key="vehicle.id"
+                          :lat-lng="getLatLngFromVehicleInfo(vehicle)"
+                          :icon="busIcon"
+                          :options="{vehicleLine:vehicle.line, vehicleType: vehicle.type}">
                     <l-tooltip :options="{permanent: true}">
                         {{vehicle.line}}
                     </l-tooltip>
@@ -13,9 +16,11 @@
                         {{vehicle}}
                     </l-popup>
                 </l-marker>
-                <l-marker v-else-if="vehicle.type===2" v-bind:key="vehicle.id"
+                <l-marker v-else-if="vehicle.type===2"
+                          :key="vehicle.id"
                           :lat-lng="getLatLngFromVehicleInfo(vehicle)"
-                          v-bind:icon="tramIcon">
+                          :icon="tramIcon"
+                          :options="{vehicleLine:vehicle.line, vehicleType: vehicle.type}">
                     <l-tooltip :options="{permanent: true}" :content="vehicle.line">
                     </l-tooltip>
                     <l-popup>
@@ -30,7 +35,7 @@
 
 <script>
     import {LMap, LTileLayer, LMarker, LTooltip, LPopup} from 'vue2-leaflet'
-    import {latLng, icon} from "leaflet";
+    import {latLng, icon, divIcon} from "leaflet";
     import Vue2LeafletMarkerCluster from 'vue2-leaflet-markercluster'
 
     export default {
@@ -53,11 +58,14 @@
                 iconSize: [20, 20],
                 iconAnchor: [10, 10]
             }),
-            vehicles: [{
-                latitude: 52.25265306914573,
-                longitude: 20.898388624191284,
-                line: `localhost:8282/vehicle_location?lines=`.concat(["122", "154", "133", "155"])
-            }],
+            vehicleMarkerClusterIconSpecification: {
+                iconUrl: require("../assets/VehicleClusterIcon.png"),
+                iconSize: [24, 24],
+                iconAnchor: [12, 12]
+            },
+            vehicles: [],
+            busesName:"Autobusy",
+            tramName:"Tramwaje"
         }),
         methods: {
             getLatLngFromVehicleInfo(vehicleInfo) {
@@ -68,7 +76,30 @@
             },
             updateVehicles() {
                 this.$http.get(this.getApiUrl(this.controls.lines)).then(result => this.vehicles = result.data);
-            }
+            },
+            summedClusterTooltip: function (childMarkers) {
+                return`${this.busesName}
+                    ${childMarkers.filter(marker=>marker.options.vehicleType===1)
+                        .map(marker=>`${marker.options.vehicleLine}\n`)}
+                        ${this.tramName}
+                    ${childMarkers.filter(marker=>marker.options.vehicleType===2)
+                        .map(marker=>`\n ${marker.options.vehicleLine}`)}
+                `
+            },
+            //Default functionality
+            vehicleMarkerClusterIcon: function (cluster) {
+               // console.log(cluster.getAllChildMarkers());
+               // console.log(this.summedClusterTooltip(cluster.getAllChildMarkers()));
+
+                return divIcon({
+                    className: "VehicleMarkerCluster",
+                    html: `
+                    <img src=${require('../assets/VehicleClusterIcon.png')} height="25px" width="25px" class="VehicleMarkerCluster">
+                    `,
+                    iconSize: this.vehicleMarkerClusterIconSpecification.iconSize,
+                    iconAnchor: this.vehicleMarkerClusterIconSpecification.iconAnchor,
+                })
+            },
         },
         mounted() {
             this.updateVehicles();
@@ -80,5 +111,10 @@
 <style scoped>
     @import "~leaflet.markercluster/dist/MarkerCluster.css";
     @import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
+
+    .VehicleMarkerCluster{
+        width: 25px;
+        height: 25px;
+    }
 </style>
 
